1d9c4ec4d6439a57e460dba6bf410db6c960f549
Mardel J. Juma <Mardel.mjj@gmail.com>
2022-12-08 14:42:21 -0800

    Done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print the phrase with proper grammar
+ * using printf
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+
+}
