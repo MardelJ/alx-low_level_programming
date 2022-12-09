cc551c26ff76a257b40cfd72ccc5606b5c04b8c4
Mardel J. Juma <Mardel.mjj@gmail.com>
2022-12-09 06:39:06 -0800

    Printing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print every number from 0 to 9
+ * Return: 0 it must be
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
