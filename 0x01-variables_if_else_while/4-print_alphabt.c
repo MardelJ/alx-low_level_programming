3d9f0479581fae18b6899f651237aa61bde7073f
Mardel J. Juma <Mardel.mjj@gmail.com>
2022-12-09 06:29:47 -0800

    Done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print every char excepting q and e
+ * Return: 0 as always
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
